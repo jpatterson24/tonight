3d97b6d605c9794ca8832f8b183f32bedf17b182
jpatterson24 <j_patterson24@hotmail.com>
2020-03-26 21:25:30 -0400

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,4 +1,29 @@
-package.org
 
-public class Account{
+package org;
+
+public class Account{
+    private String name;
+    private double balance;
+    public Account(String name), double balance  { 
+        this.name = name;
+        if(balance >0.0){
+            this.balance = balance;
+            }
+        }
+    public void deposit(double depositAmount){
+        if (depositAmount > 0.0 {
+            balance = balance + depositAmount;
+        }
+    }
+    public double getBalance(){
+        return balance;
+    }
+    publice void setName(String name){
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name){this.name = name;}
+    public String getName(){ return name; }
 }
