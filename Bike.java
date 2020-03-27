6753c415a54ad8062d5367711aa11e1d0108f5ac
jpatterson24 <j_patterson24@hotmail.com>
2020-03-26 21:27:10 -0400

    Update Bike.java

--- Bike.java
+++ Bike.java
@@ -1,3 +1,18 @@
-package org;
-public class Bike{
+package org;
+
+public class Bike{
+    private int size;
+    private String name;
+
+    public void ride() {
+        System.out.println("riding " + name);
+    }
+
+    public static void main (String arg[]){
+
+        Bike firstBike = new Bike();
+        firstBike.name = "road";
+        firstBike.size = 24;
+        firstBike.ride();
+    }
 }
